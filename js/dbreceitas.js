767f6f558432a16458e60f9038a724bbbab3d787
Letícia de Melo Azevedo <leleazevedonovo@gmail.com>
2023-11-20 20:27:03 -0300

    criação do layout principal, arquivo de dados e estilização css

---
+++ js/dbreceitas.js
@@ -0,0 +1,40 @@
+const receitas=[ 
+    { 
+        "id":1,
+        "destaque":1,
+        "categoria":"doces",
+        "nome":"brigadeiro",
+        "tumb":"img-brigadeiro.jpg",
+        "ingredientes":['1 caixa de leite condensado','1 colher (sopa) de margarina sem sal','7 colheres (sopa) de achocolatado ou 4 colheres (sopa) de chocolate em pó','chocolate granulado'],
+        "preparo":`<ul>
+        <li>Em uma panela funda, acrescente o leite condensado, a margarina e o chocolate em pó.</li>
+        <li>Cozinhe em fogo médio e mexa até que o brigadeiro comece a desgrudar da panela.</li>
+        <li>Deixe esfriar e faça pequenas bolas com a mão passando a massa no chocolate granulado.</li>
+        </ul>`
+    },
+    { 
+        "id":2,
+        "destaque":0,
+        "categoria":"doces",
+        "nome":"pudim",
+        "tumb":"img-pudim.jpg",
+        "conteudo":"Aqui vai a receita"
+    },
+    { 
+        "id":2,
+        "destaque":0,
+        "categoria":"salgado",
+        "nome":"pudim",
+        "tumb":"img-pudim.jpg",
+        "conteudo":"Aqui vai a receita"
+    },
+    { 
+        "id":2,
+        "destaque":0,
+        "categoria":"doces",
+        "nome":"pudim",
+        "tumb":"img-pudim.jpg",
+        "conteudo":"Aqui vai a receita"
+    }
+]
+export default receitas
---
+++ js/index.js
@@ -0,0 +1,2 @@
+import carregarReceitas from "./app.js";
+carregarReceitas()
