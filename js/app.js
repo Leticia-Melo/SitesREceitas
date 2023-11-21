45fab049f50d77d83a4d1f212e01a3bc98f05f11
Letícia de Melo Azevedo <leleazevedonovo@gmail.com>
2023-11-21 15:01:13 -0300

    criação das rotas home, doces e salgados

--- js/app.js
+++ js/app.js
@@ -1,4 +1,7 @@
 import receitas from "./dbreceitas.js";
+let buttonHome=document.querySelector('[data-home]')
+let buttonDoces=document.querySelector('[data-doces]')
+let buttonSalgados=document.querySelector('[data-salgados]')
 let conteinerReceitas=document.querySelector('[data-conteiner-receitas]')
 function carregarReceitas(){
     receitas.forEach(item => {
@@ -17,4 +20,48 @@ function carregarReceitas(){
             `    
     });
 }
+buttonHome.addEventListener("click",()=>{
+    conteinerReceitas.innerHTML=""
+    return carregarReceitas()
+})
+buttonDoces.addEventListener("click",()=>{
+    conteinerReceitas.innerHTML=""
+    receitas.forEach(item => {
+        if(item.categoria=="doces"){
+            conteinerReceitas.innerHTML+=` 
+            <div class="card-receita">
+                    <div class="conteudo-receita">
+                        <h1>${item.nome}</h1>
+                        <picture class="tumb">
+                            <img src="assets/img/${item.tumb}">
+                        </picture>
+                        <div class="ver-receita">
+                            <button>Ver Receita</button>
+                        </div>
+                    </div>
+                </div>
+            `    
+        }
+});
+})
+buttonSalgados.addEventListener("click",()=>{
+    conteinerReceitas.innerHTML=""
+    receitas.forEach(item => {
+        if(item.categoria=="salgados"){
+            conteinerReceitas.innerHTML+=` 
+            <div class="card-receita">
+                    <div class="conteudo-receita">
+                        <h1>${item.nome}</h1>
+                        <picture class="tumb">
+                            <img src="assets/img/${item.tumb}">
+                        </picture>
+                        <div class="ver-receita">
+                            <button>Ver Receita</button>
+                        </div>
+                    </div>
+                </div>
+            `    
+        }
+});
+})
 export default carregarReceitas
